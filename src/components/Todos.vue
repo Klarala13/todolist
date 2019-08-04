77cd33101499bd3ffa93fb0661e0a4b800372438
Klara <sanchez.clara13@gmail.com>
2019-08-04 21:08:02 +0200

    created Todos component

---
+++ src/components/Todos.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div v-bind:key="todo.id" v-for="todo in todos">
+      <Todoitem v-bind:todo="todo" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Todoitem from "./Todoitem.vue";
+export default {
+  name: "ToDos",
+  components: {
+    Todoitem
+  },
+  props:[
+    "todos"
+  ]
+}
+</script>
+
+<style scoped>
+</style>
