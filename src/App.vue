d43a4c99e348a9059d7f910b7de9dca5d2c734a6
Klara <sanchez.clara13@gmail.com>
2019-08-05 14:55:21 +0200

    delete method by id

--- src/App.vue
+++ src/App.vue
@@ -27,7 +27,9 @@ export default {
   },
   methods:{
     deleteTodo(id) {
-      this.todos = this.todos.filter(todo => todo.id !== id);
+      axios.delete(`https://jsonplaceholder.typicode.com/todos${id}`)
+      .then(res => this.todos = this.todos.filter(todo => todo.id !== id);)
+      .catch(err => console.error(err));
     },
     addTodo(newTodo) {
       const { title, completed } = newTodo;
