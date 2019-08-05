8d5945ad276b5b4bc7a41c3106065edddf0fb902
Klara <sanchez.clara13@gmail.com>
2019-08-05 13:30:40 +0200

    global style for btn

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <Header />
+    <AddTodo />
     <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
   </div>
 </template>
@@ -8,10 +9,12 @@
 <script>
 import Header from "./components/layout/Header";
 import Todos from "./components/Todos";
+import AddTodo from "./components/AddTodo";
 export default {
   name: 'app',
   components: {
     Header,
+    AddTodo,
     Todos
   },
   data(){
@@ -52,5 +55,16 @@ export default {
 body{
   font-family: Arial, Helvetica, sans-serif;
   line-height: 1.5;
+}
+.btn{
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px, 20px;
+  cursor: pointer;
+}
+.btn:hover {
+  background: #666;
 }
 </style>
